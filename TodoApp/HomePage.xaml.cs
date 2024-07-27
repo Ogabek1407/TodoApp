f1acc330edd31b73a7857266b18b8975b8416b17
Ogabek1407 <oagbeksaparov1407@gmail.com>
2024-07-28 00:39:07 +0500

    Create home page

---
+++ TodoApp/HomePage.xaml.cs
@@ -0,0 +1,9 @@
+namespace TodoApp;
+
+public partial class HomePage : ContentPage
+{
+	public HomePage()
+	{
+		InitializeComponent();
+	}
+}
---
+++ TodoApp/ProfilePage.xaml.cs
@@ -0,0 +1,9 @@
+namespace TodoApp;
+
+public partial class ProfilePage : ContentPage
+{
+	public ProfilePage()
+	{
+		InitializeComponent();
+	}
+}
